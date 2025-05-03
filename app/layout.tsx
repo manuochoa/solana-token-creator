bdc701642047f6cbffe0543b0bcb86cc67382002
manu_8a <manu_8a@outlook.com>
2025-05-02 22:41:12 -0400

    add site metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,24 @@
 import type React from "react";
 import type { Metadata } from "next";
 import "./globals.css";
+import { siteConfig } from "@/config/site";
 
 export const metadata: Metadata = {
-  title: "Token Minting Dapp",
-  description: "Mint Solana tokens easily",
-  generator: "Fuego Labz",
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  applicationName: siteConfig.name,
+  authors: siteConfig.authors,
+  creator: siteConfig.creator,
+  publisher: siteConfig.publisher,
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: siteConfig.openGraph,
+  twitter: siteConfig.twitter,
+  keywords: siteConfig.keywords,
 };
 
 export default function RootLayout({
---
+++ config/site.ts
@@ -0,0 +1,54 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Solana Token Creator",
+  description:
+    "A user-friendly web application for creating and distributing SPL tokens using the **Token-2022** program on the Solana blockchain, with integrated support for on-chain metadata and permanent storage.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://x.com/fuegolabz",
+    docs: "",
+  },
+  authors: [{ name: "Fuego Labz", url: "https://fuegolabz.io" }],
+  creator: "Fuego Labz",
+  publisher: "Fuego Labz",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "Solana Token Creator",
+    description:
+      "A user-friendly web application for creating and distributing SPL tokens using the **Token-2022** program on the Solana blockchain, with integrated support for on-chain metadata and permanent storage.",
+    url: "https://token-creator.fuegolabz.io",
+    siteName: "Solana Token Creator",
+    images: [{ url: "/logo.png" }],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Solana Token Creator",
+    description:
+      "A user-friendly web application for creating and distributing SPL tokens using the **Token-2022** program on the Solana blockchain, with integrated support for on-chain metadata and permanent storage.",
+    creator: "@fuegolabz",
+    images: ["/logo.png"],
+  },
+  keywords: [
+    "Solana",
+    "SPL",
+    "Token22",
+    "Token-2022",
+    "Token Creator",
+    "Token Mint",
+    "Token Metadata",
+    "Arweave",
+    "Bundlr",
+    "Fuego Labz",
+    "memecoin",
+    "crypto",
+  ],
+};
