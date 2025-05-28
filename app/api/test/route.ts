6e29fff6a77906c5c07943b483b85c364fda7f9b
manu_8a <manu_8a@outlook.com>
2025-05-28 18:37:13 -0400

    test post too

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -8,3 +8,10 @@ export async function GET(request: Request) {
 
   return NextResponse.json({ message: "pong" });
 }
+export async function POST(request: Request) {
+  // wait 15 seconds and then return
+  console.log("request to ping endpoint GET");
+  await new Promise((resolve) => setTimeout(resolve, 25000));
+
+  return NextResponse.json({ message: "pong" });
+}
