54123b92de9024e8bd2d5a77f9dfa3a9bb39821b
manu_8a <manu_8a@outlook.com>
2025-05-28 18:53:29 -0400

    increase timeout

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,6 +1,8 @@
 // ping endpoint
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300; // This function can run for a maximum of 5 minutes
+
 export async function GET(request: Request) {
   // wait 15 seconds and then return
   console.log("request to ping endpoint GET");
